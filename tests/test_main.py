345a2211f7b623fe62d49707afa2c7abb1053836
michal3 <michal.brzezinski>
2024-11-22 12:49:47 +0100

    insignificant changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,13 @@ import pytest
 from hypothesis import given, settings
 from hypothesis.strategies import integers
 
+
 @given(a=integers(min_value=-100, max_value=100),
        b=integers(min_value=-100, max_value=100))
 @settings(max_examples=10)
-
-
-def test_smth_else (a,b):
+def test_smth_else(a, b):
     assert func(a, b) == a + b
+
 
 @pytest.mark.parametrize(
     ("x", "y", "expected"),
