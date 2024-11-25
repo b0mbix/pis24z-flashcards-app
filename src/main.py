62dc5cc1028acf63eb036c7a1e6cd9cf340f73d6
michal3 <michal.brzezinski>
2024-11-25 15:15:27 +0100

    adding requirements.txt file

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
+
 def func(x, y):
     return x + y
 
- 
+
 if __name__ == '__main__':
     execute_from_command_line()
-   
+
     print("Hello world")
