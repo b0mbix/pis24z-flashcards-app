35057ad1b92b464a77658a5330bd97c9ae7be66d
Kacper Straszak <k3acper@o2.pl>
2024-12-24 02:09:59 +0100

    PIS-50 models of elements present in the database have been implemented

--- src/logic/api/models.py
+++ src/logic/api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    is_premium = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class FlashcardSet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='flashcard_sets')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.IntegerField(default=0)
+    is_archived = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Flashcard(models.Model):
+    set = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name='flashcards')
+    question = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question[:50]  # Zwraca pierwsze 50 znaków pytania
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FlashcardSetTag(models.Model):
+    set = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE, related_name='tags')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='flashcard_sets')
+
+    def __str__(self):
+        return f"{self.set.name} - {self.tag.name}"
+
