645e45be82233e12ec3feff25ccff496ff0f1464
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2025-01-11 15:35:29 +0100

    localhost?

--- src/logic/backend/settings.py
+++ src/logic/backend/settings.py
@@ -36,7 +36,7 @@ else:  # domyślnie "green"
             'NAME': 'flashcards_db',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
-            'HOST': '0.0.0.0',
+            'HOST': 'localhost',
             'PORT': '5432',  # Port dla środowiska green
         }
     }
