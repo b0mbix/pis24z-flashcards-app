e8153d15b901c07d92470ef6199160bef6ef9243
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2025-01-25 12:46:19 +0100

    Revert "updated dockerfile"
    This reverts commit 4135934abe044437451428695a443f12a534b8df.

--- src/gui/flutter/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.fiszki
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- src/gui/flutter/lib/index.dart
+++
@@ -1,9 +0,0 @@
-// Export pages
-export '/pages/add_cards/add_cards_widget.dart' show AddCardsWidget;
-export '/pages/learning_page/learning_page_widget.dart' show LearningPageWidget;
-export '/pages/auth/start_screen/start_screen_widget.dart'
-    show StartScreenWidget;
-export '/pages/auth/create_account/create_account_widget.dart'
-    show CreateAccountWidget;
-export '/pages/auth/login/login_widget.dart' show LoginWidget;
-export '/main_page/main_page_widget.dart' show MainPageWidget;
