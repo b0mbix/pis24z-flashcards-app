7b8a7c5fce1659d9a2b41bde0dd2b689f000b728
Kacper Straszak <k3acper@o2.pl>
2025-01-25 15:46:25 +0100

    alternative databases implemented

--- database/init.sql
+++ database/init.sql
@@ -54,22 +54,41 @@ CREATE TABLE FlashcardSetStats (
     user_id INT REFERENCES Users(id),
     set_id INT REFERENCES FlashcardSets(id),
     flashcards_viewed INT DEFAULT 0,
-    total_study_time INTERVAL DEFAULT INTERVAL '0 seconds',
-    correct_answers INT DEFAULT 0,
-    incorrect_answers INT DEFAULT 0,
-    completion_percentage INT DEFAULT 0 
+    total_study_time INTERVAL DEFAULT INTERVAL '0 seconds'
 );
 
 CREATE TABLE FlashcardStats (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES Users(id),
     flashcard_id INT REFERENCES Flashcards(id),
+    mode VARCHAR(20) NOT NULL CHECK (mode IN ('simple', 'stages', 'percent')),
     view_count INT DEFAULT 0,
-    correct_answers INT DEFAULT 0,
-    incorrect_answers INT DEFAULT 0,
-    total_study_time INTERVAL DEFAULT INTERVAL '0 seconds',
-    last_answered_at TIMESTAMP,
-    learning_stage VARCHAR(20) DEFAULT 'not_learned' -- Możliwe wartości: 'not_learned', 'learning', 'almost_learned', 'mastered'
+    stage INT, -- Dla trybu 'stages'
+    learned BOOLEAN, -- Dla trybu 'stages'
+    learning_stage VARCHAR(20) CHECK (learning_stage IN ('not_learned', 'still_learning', 'almost_learned', 'learned')) DEFAULT 'not_learned', -- Dla trybu 'percent'
+    total_study_time INTERVAL DEFAULT INTERVAL '0 seconds'
 );
 
+CREATE TABLE FlashcardStatsSimple (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(id),
+    flashcard_id INT REFERENCES Flashcards(id),
+    view_count INT DEFAULT 0,
+);
 
+CREATE TABLE FlashcardStatsStages (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(id),
+    flashcard_id INT REFERENCES Flashcards(id),
+    view_count INT DEFAULT 0,
+    stage INT DEFAULT 1,
+    learned BOOLEAN DEFAULT 0
+);
+
+CREATE TABLE FlashcardStatsPercent (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(id),
+    flashcard_id INT REFERENCES Flashcards(id),
+    view_count INT DEFAULT 0,
+    learning_stage VARCHAR(20) CHECK (learning_stage IN ('not_learned', 'still_learning', 'almost_learned', 'learned')) DEFAULT 'not_learned' 
+);
