55d2d0cedd5f35b3394a61ecb611989177d0e1df
Kacper Straszak <k3acper@o2.pl>
2025-01-28 04:09:16 +0100

    linter

--- src/logic/api/tests/test_models.py
+++ src/logic/api/tests/test_models.py
@@ -1,13 +1,11 @@
 import pytest
-from django.db.utils import IntegrityError
+from api.models import (Flashcard, FlashcardFavorite, FlashcardSet,
+                        FlashcardSetFavorite, FlashcardSetStats,
+                        FlashcardSetTag, FlashcardStatsPercent,
+                        FlashcardStatsSimple, FlashcardStatsStages, Tag)
 from django.contrib.auth.models import User
-from api.models import (
-    FlashcardSet, Flashcard, Tag, FlashcardSetTag, FlashcardSetFavorite,
-    FlashcardFavorite, FlashcardSetStats, FlashcardStatsSimple, FlashcardStatsStages,
-    FlashcardStatsPercent
-)
-from datetime import timedelta
 from django.db import connections
+from django.db.utils import IntegrityError
 
 
 @pytest.fixture(autouse=True)
@@ -110,9 +108,13 @@ def test_str_tag():
 @pytest.mark.django_db(transaction=True)
 def test_create_flashcard_set_tag():
     user = User.objects.create(username="tomasz666", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z programowania")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z programowania"
+        )
     tag = Tag.objects.create(name="Python")
-    flashcard_set_tag = FlashcardSetTag.objects.create(set=flashcard_set, tag=tag)
+    flashcard_set_tag = FlashcardSetTag.objects.create(
+        set=flashcard_set, tag=tag
+        )
 
     assert flashcard_set_tag.set == flashcard_set
     assert flashcard_set_tag.tag == tag
@@ -122,7 +124,9 @@ def test_create_flashcard_set_tag():
 @pytest.mark.django_db(transaction=True)
 def test_unique_tag_for_flashcard_set():
     user = User.objects.create(username="tomasz234", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z literatury")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z literatury"
+        )
     tag = Tag.objects.create(name="Renesans")
 
     FlashcardSetTag.objects.create(set=flashcard_set, tag=tag)
@@ -142,7 +146,9 @@ def test_create_tag_for_nonexistent_flashcard_set():
 @pytest.mark.django_db(transaction=True)
 def test_create_tag_for_nonexistent_tag():
     user = User.objects.create(username="bartek567", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z języków")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z języków"
+        )
 
     with pytest.raises(IntegrityError):
         FlashcardSetTag.objects.create(set=flashcard_set, tag_id=999)
@@ -151,9 +157,15 @@ def test_create_tag_for_nonexistent_tag():
 @pytest.mark.django_db(transaction=True)
 def test_create_flashcard_stats_simple():
     user = User.objects.create(username="bartekADP", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z angielskiego")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="What is an apple?", answer="A fruit.")
-    stats = FlashcardStatsSimple.objects.create(user=user, flashcard=flashcard, view_count=10)
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z angielskiego"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="What is an apple?", answer="A fruit."
+        )
+    stats = FlashcardStatsSimple.objects.create(
+        user=user, flashcard=flashcard, view_count=10
+        )
 
     assert stats.user == user
     assert stats.flashcard == flashcard
@@ -163,31 +175,59 @@ def test_create_flashcard_stats_simple():
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_stats_simple_str():
     user = User.objects.create(username="bartekCBD", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z angielskiego")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="What is an apple?", answer="A fruit.")
-    stats = FlashcardStatsSimple.objects.create(user=user, flashcard=flashcard, view_count=5)
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z angielskiego"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="What is an apple?", answer="A fruit."
+        )
+    stats = FlashcardStatsSimple.objects.create(
+        user=user, flashcard=flashcard, view_count=5
+        )
 
-    expected_str = f"{user.username} - {flashcard.id} (Views: {stats.view_count})"
+    expected_str = (f"{user.username} - {flashcard.id} "
+                    f" (Views: {stats.view_count})"
+                    )
     assert str(stats) == expected_str
 
 
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_stats_percent_unique_constraint():
     user = User.objects.create(username="bartekAGD", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z biologii")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Co to jest fotosynteza?", answer="Proces produkcji energii.")
-    FlashcardStatsPercent.objects.create(user=user, flashcard=flashcard, view_count=2, learning_stage="still_learning")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z biologii"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, 
+        question="Co to jest fotosynteza?", 
+        answer="Proces produkcji energii."
+        )
+    FlashcardStatsPercent.objects.create(
+        user=user, 
+        flashcard=flashcard, 
+        view_count=2, 
+        learning_stage="still_learning"
+        )
     
     # Próba stworzenia duplikatu powinna rzucić wyjątek
     with pytest.raises(IntegrityError):
-        FlashcardStatsPercent.objects.create(user=user, flashcard=flashcard, view_count=2, learning_stage="learned")
+        FlashcardStatsPercent.objects.create(
+            user=user, 
+            flashcard=flashcard, 
+            view_count=2, 
+            learning_stage="learned"
+            )
 
 
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_set_favorite():
     user = User.objects.create(username="bartekDLL", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z chemii")
-    favorite = FlashcardSetFavorite.objects.create(user=user, set=flashcard_set)
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z chemii"
+        )
+    favorite = FlashcardSetFavorite.objects.create(
+        user=user, set=flashcard_set
+        )
 
     assert favorite.user == user
     assert favorite.set == flashcard_set
@@ -197,8 +237,14 @@ def test_flashcard_set_favorite():
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_favorite():
     user = User.objects.create(username="bartekMLG", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z biologii")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Co to jest DNA?", answer="Nośnik informacji genetycznej")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z biologii"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, 
+        question="Co to jest DNA?", 
+        answer="Nośnik informacji genetycznej"
+        )
     favorite = FlashcardFavorite.objects.create(user=user, flashcard=flashcard)
 
     assert favorite.user == user
@@ -209,39 +255,66 @@ def test_flashcard_favorite():
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_set_stats():
     user = User.objects.create(username="bartekPDF", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z fizyki")
-    stats = FlashcardSetStats.objects.create(user=user, set=flashcard_set, flashcards_viewed=5)
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z fizyki"
+        )
+    stats = FlashcardSetStats.objects.create(
+        user=user, set=flashcard_set, flashcards_viewed=5
+        )
 
     assert stats.user == user
     assert stats.set == flashcard_set
     assert stats.flashcards_viewed == 5
-    assert str(stats) == f"{stats.id} - set: {flashcard_set.id} - user: {user.id}"
+    assert str(stats) == (f"{stats.id} - set:" 
+                          f" {flashcard_set.id} - user: {user.id}"
+                          )
 
 
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_stats_stages():
     user = User.objects.create(username="bartekJPG", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z matematyki")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Ile to 2+2?", answer="4")
-    stats = FlashcardStatsStages.objects.create(user=user, flashcard=flashcard, view_count=3, stage=2, learned=True)
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z matematyki"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Ile to 2+2?", answer="4"
+        )
+    stats = FlashcardStatsStages.objects.create(
+        user=user, flashcard=flashcard, view_count=3, stage=2, learned=True
+        )
 
     assert stats.user == user
     assert stats.flashcard == flashcard
     assert stats.view_count == 3
     assert stats.stage == 2
     assert stats.learned is True
-    assert str(stats) == f"{user.username} - {flashcard.id} (Stage: {stats.stage}, Learned: {stats.learned})"
+    assert str(stats) == (f"{user.username} - {flashcard.id} " 
+                          f" (Stage: {stats.stage}, Learned: {stats.learned})"
+                          )
 
 
 @pytest.mark.django_db(transaction=True)
 def test_flashcard_stats_percent():
     user = User.objects.create(username="bartekPNG", password="password123")
-    flashcard_set = FlashcardSet.objects.create(user=user, name="Fiszki z języka polskiego")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Co to jest metafora?", answer="Środek stylistyczny")
-    stats = FlashcardStatsPercent.objects.create(user=user, flashcard=flashcard, view_count=2, learning_stage="still_learning")
+    flashcard_set = FlashcardSet.objects.create(
+        user=user, name="Fiszki z języka polskiego"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, 
+        question="Co to jest metafora?", 
+        answer="Środek stylistyczny"
+        )
+    stats = FlashcardStatsPercent.objects.create(
+        user=user, 
+        flashcard=flashcard, 
+        view_count=2, 
+        learning_stage="still_learning"
+        )
 
     assert stats.user == user
     assert stats.flashcard == flashcard
     assert stats.view_count == 2
     assert stats.learning_stage == "still_learning"
-    assert str(stats) == f"{user.username} - {flashcard.id} (Stage: {stats.learning_stage})"
+    assert str(stats) == (f"{user.username} - {flashcard.id} "
+                          f"(Stage: {stats.learning_stage})"
+                          )
--- src/logic/api/tests/test_views.py
+++ src/logic/api/tests/test_views.py
@@ -1,21 +1,24 @@
-import pytest
-from django.urls import reverse
-from rest_framework.test import APIClient
 from datetime import timedelta
+
+import pytest
+from api.models import (Flashcard, FlashcardFavorite, FlashcardSet,
+                        FlashcardSetFavorite, FlashcardSetStats,
+                        FlashcardSetTag, FlashcardStatsPercent,
+                        FlashcardStatsSimple, FlashcardStatsStages, Tag, User)
+from django.urls import reverse
 from rest_framework import status
-from api.models import (
-    User, FlashcardSet, Flashcard, Tag, FlashcardSetTag,
-    FlashcardSetFavorite, FlashcardFavorite, FlashcardSetStats,
-    FlashcardStatsSimple, FlashcardStatsStages, FlashcardStatsPercent
-)
+from rest_framework.test import APIClient
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
+
 @pytest.fixture
 def test_user():
     return User.objects.create(username="test_user", password="password123")
+
 
 @pytest.mark.django_db
 def test_register_user(api_client):
@@ -29,6 +32,7 @@ def test_register_user(api_client):
     assert "user_id" in response.data
     assert User.objects.filter(username="new_user").exists()
 
+
 @pytest.mark.django_db
 def test_register_user_invalid(api_client):
     url = reverse('register')
@@ -38,10 +42,11 @@ def test_register_user_invalid(api_client):
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
+
 @pytest.mark.django_db
 def test_login_user(api_client, test_user):
-    test_user.set_password("password123")  # Ustaw hasło w odpowiednim formacie
-    test_user.save()  # Zapisz zmiany w użytkowniku
+    test_user.set_password("password123")  
+    test_user.save()  
 
     url = reverse('login')
     data = {
@@ -52,6 +57,7 @@ def test_login_user(api_client, test_user):
     assert response.status_code == status.HTTP_200_OK
     assert "access" in response.data
 
+
 @pytest.mark.django_db
 def test_login_user_invalid(api_client):
     url = reverse('login')
@@ -61,6 +67,7 @@ def test_login_user_invalid(api_client):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
 
 @pytest.mark.django_db
 def test_add_flashcard_set(api_client, test_user):
@@ -75,11 +82,16 @@ def test_add_flashcard_set(api_client, test_user):
     assert "set_id" in response.data
     assert FlashcardSet.objects.filter(name="Test Flashcards").exists()
 
+
 @pytest.mark.django_db
 def test_get_flashcard_sets(api_client, test_user):
-    FlashcardSet.objects.all().delete()  # Wyczyszczenie bazy przed testem
-    FlashcardSet.objects.create(user=test_user, name="Set 1", description="Description 1")
-    FlashcardSet.objects.create(user=test_user, name="Set 2", description="Description 2")
+    FlashcardSet.objects.all().delete()
+    FlashcardSet.objects.create(
+        user=test_user, name="Set 1", description="Description 1"
+        )
+    FlashcardSet.objects.create(
+        user=test_user, name="Set 2", description="Description 2"
+        )
 
     url = reverse('get_flashcard_sets')
     response = api_client.get(url)
@@ -88,9 +100,12 @@ def test_get_flashcard_sets(api_client, test_user):
     assert response.data[0]["name"] == "Set 1"
     assert response.data[1]["name"] == "Set 2"
 
+
 @pytest.mark.django_db
 def test_get_flashcard_set_details(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Details Set", description="Detailed description")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Details Set", description="Detailed description"
+        )
     Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
     tag = Tag.objects.create(name="Tag1")
     FlashcardSetTag.objects.create(set=flashcard_set, tag=tag)
@@ -103,6 +118,7 @@ def test_get_flashcard_set_details(api_client, test_user):
     assert response.data["flashcards"][0]["question"] == "Q1"
     assert response.data["tags"][0]["name"] == "Tag1"
 
+
 @pytest.mark.django_db
 def test_add_tag(api_client):
     url = reverse('add_tag')
@@ -112,6 +128,7 @@ def test_add_tag(api_client):
     assert "tag_id" in response.data
     assert Tag.objects.filter(name="New Tag").exists()
 
+
 @pytest.mark.django_db
 def test_get_tag(api_client):
     tag = Tag.objects.create(name="Existing Tag")
@@ -119,6 +136,7 @@ def test_get_tag(api_client):
     response = api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     assert response.data["name"] == "Existing Tag"
+
 
 @pytest.mark.django_db
 def test_update_tag(api_client):
@@ -129,6 +147,7 @@ def test_update_tag(api_client):
     assert response.status_code == status.HTTP_200_OK
     assert Tag.objects.get(id=tag.id).name == "Updated Name"
 
+
 @pytest.mark.django_db
 def test_delete_tag(api_client):
     tag = Tag.objects.create(name="To Delete")
@@ -137,9 +156,12 @@ def test_delete_tag(api_client):
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert not Tag.objects.filter(id=tag.id).exists()
 
+
 @pytest.mark.django_db
 def test_add_flashcard(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Set for Flashcard")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Set for Flashcard"
+        )
     url = reverse('add_flashcard')
     data = {
         "set_id": flashcard_set.id,
@@ -150,18 +172,25 @@ def test_add_flashcard(api_client, test_user):
     assert response.status_code == status.HTTP_201_CREATED
     assert Flashcard.objects.filter(question="What is Python?").exists()
 
+
 @pytest.mark.django_db
 def test_delete_flashcard(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Set for Delete Test")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Set for Delete Test"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
     url = reverse('delete_flashcard', args=[flashcard.id])
     response = api_client.delete(url)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert not Flashcard.objects.filter(id=flashcard.id).exists()
 
+
 @pytest.mark.django_db
 def test_add_flashcard_set_to_favorites(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Favorite Set")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Favorite Set")
     url = reverse('add_flashcard_set_to_favorites')
     data = {
         "user_id": test_user.id,
@@ -169,12 +198,19 @@ def test_add_flashcard_set_to_favorites(api_client, test_user):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardSetFavorite.objects.filter(user=test_user, set=flashcard_set).exists()
+    assert FlashcardSetFavorite.objects.filter(
+        user=test_user, set=flashcard_set
+        ).exists()
+
 
 @pytest.mark.django_db
 def test_add_flashcard_to_favorites(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Set with Flashcard")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Set with Flashcard"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
     url = reverse('add_flashcard_to_favorites')
     data = {
         "user_id": test_user.id,
@@ -182,45 +218,56 @@ def test_add_flashcard_to_favorites(api_client, test_user):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardFavorite.objects.filter(user=test_user, flashcard=flashcard).exists()
+    assert FlashcardFavorite.objects.filter(
+        user=test_user, flashcard=flashcard
+        ).exists()
 
 
 @pytest.mark.django_db
 def test_add_flashcard_set_stats(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Stats Set")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Stats Set"
+        )
     url = reverse('add_flashcard_set_stats')
     data = {
         "user_id": test_user.id,
         "set_id": flashcard_set.id,
         "flashcards_viewed": 5,
-        "total_study_time": timedelta(seconds=120).total_seconds() 
+        "total_study_time": timedelta(seconds=120).total_seconds()
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardSetStats.objects.filter(set=flashcard_set, user=test_user).exists()
+    assert FlashcardSetStats.objects.filter(
+        set=flashcard_set, user=test_user
+        ).exists()
 
-from datetime import timedelta
 
 @pytest.mark.django_db
 def test_get_flashcard_set_stats(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Stats Set")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Stats Set"
+        )
     stats = FlashcardSetStats.objects.create(
         user=test_user,
         set=flashcard_set,
         flashcards_viewed=10,
-        total_study_time=timedelta(seconds=300)  # Użyj timedelta
+        total_study_time=timedelta(seconds=300)
     )
     url = reverse('get_flashcard_set_stats', args=[stats.id])
     response = api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     assert response.data["flashcards_viewed"] == 10
-    assert response.data["total_study_time"] == 300  # Oczekuj wyniku w sekundach
+    assert response.data["total_study_time"] == 300
 
 
 @pytest.mark.django_db
 def test_add_flashcard_stats_simple(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Simple Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Simple Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
     url = reverse('add_flashcard_stats_simple')
     data = {
         "user_id": test_user.id,
@@ -229,13 +276,22 @@ def test_add_flashcard_stats_simple(api_client, test_user):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardStatsSimple.objects.filter(user=test_user, flashcard=flashcard).exists()
+    assert FlashcardStatsSimple.objects.filter(
+        user=test_user, flashcard=flashcard
+        ).exists()
+
 
 @pytest.mark.django_db
 def test_update_flashcard_stats_simple(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Simple Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
-    stats = FlashcardStatsSimple.objects.create(user=test_user, flashcard=flashcard, view_count=2)
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Simple Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
+    stats = FlashcardStatsSimple.objects.create(
+        user=test_user, flashcard=flashcard, view_count=2
+        )
     url = reverse('update_flashcard_stats_simple', args=[stats.id])
     data = {
         "view_count": 5
@@ -244,10 +300,15 @@ def test_update_flashcard_stats_simple(api_client, test_user):
     assert response.status_code == status.HTTP_200_OK
     assert FlashcardStatsSimple.objects.get(id=stats.id).view_count == 5
 
+
 @pytest.mark.django_db
 def test_add_flashcard_stats_stages(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Stages Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Stages Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
     url = reverse('add_flashcard_stats_stages')
     data = {
         "user_id": test_user.id,
@@ -258,13 +319,26 @@ def test_add_flashcard_stats_stages(api_client, test_user):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardStatsStages.objects.filter(user=test_user, flashcard=flashcard).exists()
+    assert FlashcardStatsStages.objects.filter(
+        user=test_user, flashcard=flashcard
+        ).exists()
+
 
 @pytest.mark.django_db
 def test_update_flashcard_stats_stages(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Stages Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
-    stats = FlashcardStatsStages.objects.create(user=test_user, flashcard=flashcard, view_count=1, stage=1, learned=False)
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Stages Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
+    stats = FlashcardStatsStages.objects.create(
+        user=test_user,
+        flashcard=flashcard,
+        view_count=1,
+        stage=1,
+        learned=False
+        )
     url = reverse('update_flashcard_stats_stages', args=[stats.id])
     data = {
         "view_count": 3,
@@ -278,10 +352,15 @@ def test_update_flashcard_stats_stages(api_client, test_user):
     assert stats.stage == 4
     assert stats.learned is True
 
+
 @pytest.mark.django_db
 def test_add_flashcard_stats_percent(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Percent Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Percent Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
     url = reverse('add_flashcard_stats_percent')
     data = {
         "user_id": test_user.id,
@@ -291,13 +370,25 @@ def test_add_flashcard_stats_percent(api_client, test_user):
     }
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert FlashcardStatsPercent.objects.filter(user=test_user, flashcard=flashcard).exists()
+    assert FlashcardStatsPercent.objects.filter(
+        user=test_user, flashcard=flashcard
+        ).exists()
+
 
 @pytest.mark.django_db
 def test_update_flashcard_stats_percent(api_client, test_user):
-    flashcard_set = FlashcardSet.objects.create(user=test_user, name="Percent Stats Set")
-    flashcard = Flashcard.objects.create(set=flashcard_set, question="Q1", answer="A1")
-    stats = FlashcardStatsPercent.objects.create(user=test_user, flashcard=flashcard, view_count=3, learning_stage="not_learned")
+    flashcard_set = FlashcardSet.objects.create(
+        user=test_user, name="Percent Stats Set"
+        )
+    flashcard = Flashcard.objects.create(
+        set=flashcard_set, question="Q1", answer="A1"
+        )
+    stats = FlashcardStatsPercent.objects.create(
+        user=test_user,
+        flashcard=flashcard,
+        view_count=3,
+        learning_stage="not_learned"
+        )
     url = reverse('update_flashcard_stats_percent', args=[stats.id])
     data = {
         "view_count": 10,
@@ -308,5 +399,3 @@ def test_update_flashcard_stats_percent(api_client, test_user):
     stats.refresh_from_db()
     assert stats.view_count == 10
     assert stats.learning_stage == "mastered"
-
-
