687165f93a191e0b7aa2eefe824e3a1ea540fc8b
brzoza6538 <m3.brzezinski@gmail.com>
2025-01-28 02:13:23 +0100

    test widget update

--- src/gui/flutter/test/widget_test.dart
+++ src/gui/flutter/test/widget_test.dart
@@ -9,9 +9,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fiszki/main.dart';
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp());
-//   });
-// }
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
